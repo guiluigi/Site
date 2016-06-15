93ab797155ec5905ff866ef8b9b1faff9d8e99db
Synk0 <wesleyhod.wh@gmail.com>
2016-06-15 05:49:14 -0300

    sete

---
+++ sete.lua
@@ -0,0 +1,75 @@
+return {
+		br = {
+			bonus = {
+				general_pm = '_Pronto, acabei de lhe enviar uma mensagem no privado._',
+				no_user = 'Eu nunca vi esse usuário antes neste grupo. Por Favor queira me apresentar, enviando uma mensagem dele.',
+				the_group = 'o grupo',
+				adminlist_admin_required = 'Eu não sou administrador desse grupo. *Só quando eu for nomeado um administrador, estarei podendo vizualizar a lista de administradores*.',
+				mods_list = '*Moderadores do grupo*:\n&&&1',
+				settings_header = 'Configurações atuais do *grupo*:\n\n',
+				reply = '*Responda alguém* para usar este comando, ou escreva o *nome de usuário*.',
+				too_long = 'Este texto é muito longo, não posso enviar.',
+				msg_me = '_Primeiro me envie uma mensagem no privado para que eu possa então está te respondendo._',
+				tell = '*ID do grupo*: &&&1'
+			},
+		help = {
+			private = [[Olá, *&&&1*!
+Meu nome é @&&&2, fui criada pra tornar seu grupo mais divertido.
+
+*O que posso fazer?*
+Bom, Eu estou aqui para conversar.
+
+_Clique no botão_ *"Adicionar em um grupo"*]],
+},
+		ping = {
+			pong = '*Pong*',
+			redis = '*Redis OK*',
+			clean = '*Limpo*',
+			msg_server = '*Server: *',
+			msg_date = '*Mensagem: *',
+		},
+		errors = {
+			user = '`Ocorreu um erro ao executar este comando.`\n\n*Entre no Canal* @RoboED *para ficar por dentro das atualizações do nosso querido Robô de Mimetal.*',
+			dev = 'Ocorreu um erro.\n\nExecute o comando:\n\n',
+		},
+		all = {
+			initial = '_Clique em uma das_ *opções* _desejadas para obter informações sobre:_',
+			menu_first = 'Clique nos *ícones* para mudar as *configurações do grupo.*\n_Ou use a ultima linha para configurar o anti-flood._',
+			media_first = 'Tap on a voice in the right colon to *change the setting*',
+			menu = '_Eu enviei o menu no privado._',
+			group_success = '_Eu enviei a mensagem de ajuda no privado._',
+			group_not_success = '_Você precisa iniciar uma conversa no_ *privado* _primeiro._',
+		},
+				pv = 'Esse comando está disponível apenas em grupos',
+				not_mod = 'Você *não* é um(a) moderador(a)',
+				breaks_markdown = 'Esse texto apresenta problemas com markdown.\nVerifique quantas vezes você usou * ou _ ou `',
+				not_admin = '*Ops*, Se você quer que eu administre seu grupo, me adicione como administrador primeiro.',
+				evaluate= [[*Você gosta da* [Eliana Silva](https://telegram.me/ElianaSilvaBot)?
+
+*Então nos avalie!*
+
+*1 - *`Clique no botão "Avaliar".`
+*2 - *`Em seguida toque em "Começar".`
+*3 - *`Selecione a quantidade de estrelas.`
+*4 - *`Depois diga algo sobre a` [Eliana Silva](https://telegram.me/ElianaSilvaBot) `ou clique em `/skip`.`
+
+*Pronto!*
+
+*Nós da equipe* @RoboED *agradecemos por seu apoio!*]],
+				info_bot_text = [[*Olá meu nome é* [Eliana Silva](https://telegram.me/ElianaSilvaBot)*, envie */help *para ver meus comandos.*
+
+*Desenvolvedor:*
+	[Wesley Henr](http://telegram.me/Wesley_Henr)
+
+*Suporte:*
+	[Adelson Oliveira](http://telegram.me/Delson1)
+	[Ana Jackellyne](http://telegram.me/Melzinha10)
+	[Gualter Perinho](http://telegram.me/PeraDoce)
+	[Marcos Ferreira](http://telegram.me/niqui51)
+	[Tiago Danin](http://telegram.me/TiagoDanin)
+	[Vycktor Stark](http://telegram.me/VYCKTOR_STARK)
+
+*Última atualização:* ]]..version,
+
+		},
+}
