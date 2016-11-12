ebf4455a11b5971e5c1a82b4c849e3c3908cf333
Peradoce <gperinho20@gmail.com>
2016-11-12 15:52:08 -0100

    criacao da pagina colaboradores

---
+++ html/pages/colaboradores.php
@@ -0,0 +1,22 @@
+<div class="fixed-top">
+  <div class="row">
+    <div class="col s12 light-blue darken-2 white-text second-section bots">
+      <h1>Colaboradores</h1>
+    </div>
+  </div>
+</div>
+<div class="section">
+  <div class="row second-section">
+  <div class="col s12 m4 l4">
+    <div class="card">
+          <div>
+            <img class="circle" src="template/images/gugu.jpg" width="128px" height="128px">
+          </div>
+          <div class="card-content">
+            <h3>Gualter Perinho</h3>
+            <p>Desenvolvedor do Site, 23 anos, o diferentão Português.</p>
+          </div>
+        </div>
+  </div>
+</div>
+</div>
