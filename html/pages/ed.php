0c21e50796a5cd80aa8bc81e7b37dd0806dc4289
Peradoce <gperinho20@gmail.com>
2016-11-12 12:17:28 -0100

    mudanca_tabela_ed_php

--- html/pages/ed.php
+++ html/pages/ed.php
@@ -31,8 +31,137 @@
               <tbody>
                 <tr>
                   <td>9GAG</td>
-                  <td>Imagens Aleatórias do Site 9gag</td>
+                  <td>Imagens aleatórias do site 9GAG.com.</td>
                   <td>/9gag</td>
+                </tr>
+                <tr>
+                  <td>AFK</td>
+                  <td>Decretar ausência e motivo</td>
+                  <td>/afk</td>
+                </tr>
+                <tr>
+                  <td>Calculadora</td>
+                  <td>Calcular a expressão e enviar o resultado.</td>
+                  <td>/calc <b>Expressão</b></td>
+                </tr>
+                <tr>
+                  <tr>
+                    <td>Clima</td>
+                    <td>Mostar informações sobre o clima, envie o comando e em seguida o nome da cidade ou sigla.</td>
+                    <td>/clima <b>Texto</b></td>
+                  </tr>
+                  <td>Fale</td>
+                  <td>Enviar um áudio de acordo com o texto</td>
+                  <td>/fale <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Spotify</td>
+                  <td>Para pesquisar músicas no Spotify, envie o comando e em seguida o nome da música que preferir.</td>
+                  <td>/spotify <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>RE</td>
+                  <td>Alerta de status.</td>
+                  <td>/re <b> Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Mapa</td>
+                  <td>Para localizar sua cidade/estado no mapa.</td>
+                  <td>/mapa <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Hora</td>
+                  <td> Informar que horas são em sua cidade ou local mencionado.</td>
+                  <td>/hora <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>GIF</td>
+                  <td>Enviar um Gif relacionado com o assunto mencionado.</td>
+                  <td>/gif <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Girar</td>
+                  <td>Descrição</td>
+                  <td>/girar <b>Número</b></td>
+                </tr>
+                <tr>
+                  <td>Perguntar</td>
+                  <td>Para fazer perguntas simples ao bot.</td>
+                  <td>/perguntar <b>Pergunta?</b></td>
+                </tr>
+
+                <tr>
+                  <td>Cotação</td>
+                  <td>Veja a cotação do dia: Dólar, Euro, e outras moedas.</td>
+                  <td>/cot <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Filmes</td>
+                  <td>Pesquisar informações de filmes no site IMDb.com, envie o comando e em seguida o nome do filme em inglês.</td>
+                  <td>/filme <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>IMDB</td>
+                  <td>Pesquisar por filmes Séries no site IMDb.com, envie o comando e em seguida o nome do filme ou série em inglês.</td>
+                  <td>/imdb <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Série</td>
+                  <td>Pesquisar informações de Séries no site IMDb.com, envie o comando e em seguida o nome da série em inglês.</td>
+                  <td>/serie <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Wikipédia</td>
+                  <td>Pesquisar na Wikipedia.</td>
+                  <td>/wiki <b>Texto</b></td>
+                </tr>
+
+                <tr>
+                  <td>Informações</td>
+                  <td>Mostrar algumas informações úteis sobre mim.</td>
+                  <td>/info</td>
+                </tr>
+                <tr>
+                  <td>ID</td>
+                  <td>Mostrar algumas informações básicas sobre você ou o usuário que você respondeu.</td>
+                  <td>/id</td>
+                </tr>
+                <tr>
+                  <td>IP</td>
+                  <td>Mostar informações sobre o ip ou dominio mencionado.</td>
+                  <td>/ip <b>IP/Domínio</b></td>
+                </tr>
+                <tr>
+                  <td>Ping</td>
+                  <td>Verificar ping</td>
+                  <td>/ping</td>
+                </tr>
+                <tr>
+                  <td>Stickers</td>
+                  <td>Enviar um sticker de acordo com o texto mencionado.</td>
+                  <td>/sticker <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Suporte</td>
+                  <td>Envie Bug's/Perguntas/Sugestões para nossa equipe.</td>
+                  <td>/suporte <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Trânsito</td>
+                  <td>Visualizar informações sobre o transito de São Paulo.</td>
+                  <td>/trânsito <b>Local</b></td>
+                </tr>
+                <tr>
+                </tr>
+                <tr>
+                  <td>Tradução</td>
+                  <td>Para fazer a tradução de algum texto de qualquer idioma para o português.</td>
+                  <td>/tr <b>Texto</b></td>
+                </tr>
+                <tr>
+                  <td>Youtube</td>
+                  <td>Para procurar vídeos no youtube.com, envie o comando e em seguida o nome do video que deseja procurar.</td>
+                  <td>/youtube <b>Texto</b></td>
                 </tr>
               </tbody>
             </table>
