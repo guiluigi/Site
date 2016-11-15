93fcb62a2777e8c59aa4aaf1e618d7a5df4b5896
Synk0 <wesleyhod.wh@gmail.com>
2016-11-15 03:09:50 -0200

    Identando Menu

--- html/pages/menu.php
+++ html/pages/menu.php
@@ -1,28 +1,24 @@
-<div class="navbar-fixed">
-    <nav class="light-blue darken-2 menu">
-         <div class="nav-wrapper light-blue accent-3">
-           <div class="container">
-           <a href="http://synko.com.br/" class="brand-logo logo"><img src="template/images/Logo.svg" width="32px" height="32px"></a>
-           <ul class="right hide-on-med-and-down">
-             <li><a href="http://synko.com.br/">Home</a></li>
-             <li><a href="http://synko.com.br/#bots">Bots</a></li>
-             <li><a href="http://synko.com.br/#grupos">Grupos</a></li>
-             <li><a href="?p=comandos">Comandos</a></li>
-             <li><a href="http://synko.com.br/#equipe">Equipe</a></li>
-             <li><a href="?p=colaboradores">Colaboradores</a></li>
-             <li><a href="http://synko.com.br/#contatos">Contatos</a></li>
-           </ul>
-           <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-         </div>
-      </div>
-    </nav>
- </div>
- <ul class="side-nav" id="mobile-demo">
-   <li><a href="http://synko.com.br/">Home</a></li>
-   <li><a href="http://synko.com.br/#bots">Bots</a></li>
-   <li><a href="http://synko.com.br/#grupos">Grupos</a></li>
-   <li><a href="?p=comandos">Comandos</a></li>
-   <li><a href="http://synko.com.br/#equipe">Equipe</a></li>
-   <li><a href="?p=colaboradores">Colaboradores</a></li>
-   <li><a href="http://synko.com.br/#contatos">Contatos</a></li>
- </ul>
+<nav class="menu">
+  <div class="nav-wrapper light-blue darken-2">
+      <a href="#" class="brand-logo"><img src="template/images/Logo.svg" width="32px" height="32px"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="http://synko.com.br/#">Home</a></li>
+        <li><a href="http://synko.com.br/#bots">Bots</a></li>
+        <li><a href="http://synko.com.br/#grupos">Grupos</a></li>
+        <li><a href="http://synko.com.br/?p=comandos">Comandos</a></li>
+        <li><a href="http://synko.com.br/#equipe">Equipe</a></li>
+        <li><a href="http://synko.com.br/?p=colaboradores">Colaboradores</a></li>
+        <li><a href="http://synko.com.br/#contatos">Contato</a></li>
+      </ul>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+  </div>
+</nav>
+<ul class="side-nav" id="mobile-demo">
+  <li><a href="http://synko.com.br/">Home</a></li>
+  <li><a href="http://synko.com.br/#bots">Bots</a></li>
+  <li><a href="http://synko.com.br/#grupos">Grupos</a></li>
+  <li><a href="?p=comandos">Comandos</a></li>
+  <li><a href="http://synko.com.br/#equipe">Equipe</a></li>
+  <li><a href="?p=colaboradores">Colaboradores</a></li>
+  <li><a href="http://synko.com.br/#contatos">Contatos</a></li>
+</ul>
